6793f767b1782390daeb94e9dfff124585951a54
Oluwatomisin Ajayi <tomisinajayi@gmail.com>
2024-09-04 15:59:17 +0100

    new project

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id :'1',
+        question : "what are the acccordion components?",
+        answer : "Accordion components are user interface",
+    },
+    {
+        id :'2',
+        question : "what are the acccordion components?",
+        answer : "Accordion components are user interface",
+    },
+    {
+        id :'3',
+        question : "what are the acccordion components?",
+        answer : "Accordion components are user interface",
+    },
+    {
+        id :'4',
+        question : "what are the acccordion components?",
+        answer : "Accordion components are user interface",
+    },
+];
+
+export default data;
