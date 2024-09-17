01e3f16814d992c1b52e2cbd2be13901771c2309
Oluwatomisin Ajayi <tomisinajayi@gmail.com>
2024-09-17 14:26:53 +0100

    random-color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-import Accordian from './components/accordion';
+
+import RandomColor from './components/random-color';
  
 function App() {
   return (
     <div className="App">
       {/* accordioncomponent */}
-      <Accordian/>
+      {/* <Accordian/> */}
+
+      {/* Random color component */}
+      <RandomColor/>
    
     </div>
   );
